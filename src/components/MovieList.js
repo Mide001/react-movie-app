c02ff4eb08850e45ce7680332d7294fbd4b42862
Mide001 <olamideisreal5@gmail.com>
2022-09-18 23:40:44 +0100

    Updated: Add Favourites

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,14 +7,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const MovieList = (props) => {
 
-    const FavouriteComponent = props.favouriteComponent;
+    
     return (
         <>
         {props.movies.map((movie, index) => (
             <div className='card col-md-2 m-3'>
             <img src={movie.Poster} key={index} alt='movie'></img>
             <p className='text-center'>Year: {movie.Year}</p>
-            <div className='text-center fontawesome-star' style={{color: 'blue'}} onClick={() => props.handleFavouritesClick(movie)}>
+            <div className='text-center fontawesome-star' 
+            style={{color: 'blue'}} onClick={() => 
+            props.handleFavouritesClick(movie)}>
                 Add To Favourite <FontAwesomeIcon icon={faStar} />
             </div>
             </div>
